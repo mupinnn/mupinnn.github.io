58dbf511318d7c9d66ac2b78d90b9d2ffebdf1e9
Ahmad Muwaffaq <itsmupin@gmail.com>
2020-10-14 15:35:28 +0700

    create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+
+export default function NotFound() {
+    return (
+        <Layout>
+            <div className="w-full text-center">
+                <p className="mb-3">Oops! Something you find didn't appear.</p>
+                <p>
+                    Don't cry, back to{" "}
+                    <Link to="/" className="text-blue-600 hover:text-blue-800">
+                        home
+                    </Link>{" "}
+                    and tell your mom.
+                </p>
+            </div>
+        </Layout>
+    );
+}
