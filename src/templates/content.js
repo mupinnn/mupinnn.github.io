f72aa7119d8ec8f4e7607f7425629fc57be0b676
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-06-05 11:04:04 +0700

    update content template to accept dynamic data from remark

--- src/templates/content.js
+++ src/templates/content.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 
-export default function Content({ pageContext }) {
-  const { slug } = pageContext;
+export default function Content({ data }) {
+  const { frontmatter, html } = data.markdownRemark;
 
   return (
     <Layout>
-      <h1>Template content</h1>
-      <p>Slug: {slug}</p>
+      <SEO title={frontmatter.title} description={frontmatter.description} />
+      <h1 className="mb-5 text-center text-xl">{frontmatter.title}</h1>
+      <article
+        className="prose prose-sm text-gray-800 md:prose"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </Layout>
   );
 }
+
+export const pageQuery = graphql`
+  query ContentPost($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        description
+      }
+      html
+    }
+  }
+`;
